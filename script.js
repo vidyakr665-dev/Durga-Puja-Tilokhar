d0796eeb08eb841ecaaa0829206027901ce6fb19
vidyakr665-dev <vidyakr665@gmail.com>
2026-09-23 09:23:30 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const links = document.querySelectorAll('nav a');
-links.forEach(link => {
-  link.addEventListener('click', () => {
-    document.querySelectorAll('nav a').forEach(x => x.classList.remove('active'));
-    link.classList.add('active');
-  });
-});
